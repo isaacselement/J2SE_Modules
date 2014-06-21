32c9ffef2e6b7bde70f785295d770a67d5d6d682
isaacselement <isaacselement@gmail.com>
2014-06-21 09:08:32 +0800

    initialize commit

---
+++ src/com/modules/Helper/DLog.java
@@ -0,0 +1,38 @@
+package com.modules.Helper;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class DLog {
+	
+	public static boolean isDebugingMode = true;		// Set it to no when in production
+	
+	public static void log(String message) {
+		if (! isDebugingMode) return; 
+		StackTraceElement[] elements = new Throwable().getStackTrace();
+		String classNameString = elements[1].getClassName();
+		println("\n ----- " + "[" + classNameString + "]" + "  " + message);
+	}
+	
+	
+	@SuppressWarnings("rawtypes")
+	public static void printMapKeysValues(Map map) {
+	    if (! isDebugingMode) return;
+	    Iterator iterator = map.entrySet().iterator();
+	    while (iterator.hasNext()) {
+	        Map.Entry pairs = (Map.Entry)iterator.next();
+	        String key = (String) pairs.getKey(); 
+	        Object value = pairs.getValue();
+	        println(key + " = " + value);
+	        iterator.remove(); // avoids a ConcurrentModificationException
+	    }
+	}
+	
+	
+	public static void println(String message) {
+		if (! isDebugingMode) return; 
+		System.out.println(message);
+	}
+	
+	
+}
